b1bbc78d6e553de0fd5512509db4beb45126b268
Álvaro Pintado <alvaro.pintado@alumnos.upm.es>
2025-08-29 15:00:09 +0200

    feat: calculator functions
    The calculator object functions were implemented, along with its tests.

---
+++ calculator.js
@@ -0,0 +1,23 @@
+class Calculator {
+    constructor() {
+        return 0;
+    }
+
+    add(num1, num2) {
+        return num1 + num2;
+    }
+
+    substract(num1, num2) {
+        return num1 - num2;
+    }
+
+    divide(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiply(num1, num2) {
+        return num1 * num2;
+    }
+}
+
+module.exports = new Calculator();
