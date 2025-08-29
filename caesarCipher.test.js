98a750d4966ba399880c8b831d7b71edfd5a86a4
Álvaro Pintado <alvaro.pintado@alumnos.upm.es>
2025-08-29 15:00:45 +0200

    feat: caesarCipher added
    The caesarCipher function and its test were implemented.

---
+++ caesarCipher.test.js
@@ -0,0 +1,11 @@
+const caesarCipher = require("./caesarCipher");
+
+test("Caesar Cipher", () => {
+    expect(caesarCipher("aaa", 1)).toEqual("bbb");
+    expect(caesarCipher("zzz", 1)).toEqual("aaa");
+    expect(caesarCipher("xyz", 3)).toEqual("abc");
+<<<<<<< HEAD
+=======
+    expect(caesarCipher("Hello, World!", 3)).toEqual("Khoor, Zruog!");
+>>>>>>> 34ace0d (feat: caesarCipher added)
+});
